60c8a5188a96e40c1f526fea8f41587f30b917c9
Henrique Costa <costa.henrique9577@gmail.com>
2026-05-22 15:06:59 -0300

    feat: alertas personalizados

--- usuarios/views.py
+++ usuarios/views.py
@@ -35,9 +35,7 @@ def cadastro(request):
         form = CadastroForms(request.POST)
 
         if form.is_valid():
-            if form['senha_1'].value() != form['senha_2'].value():
-                messages.error(request, 'Senhas não coincidem')
-                return redirect('cadastro')
+            
             
             nome = form['nome_cadastro'].value()
             email = form['email'].value()
